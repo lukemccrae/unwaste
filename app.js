9aa1dac27a5867a6401896c5ef0a822974b666bb
“lukemccrae” <lukemccrae@gmail.com>
2016-12-04 13:01:16 -0700

    worked on redirect page, still need to figure out how exactly hosts file works

---
+++ app.js
@@ -0,0 +1,12 @@
+$('#button').on('click', function() {
+    var lineBreak = "<br>"
+    var reRoute = "127.0.0.1         "
+    var input = $('#input').val()
+    console.log(reRoute);
+    $('#textarea').append(reRoute + input + "&#013;&#010")
+});;
+
+function textConfirm() {
+    var inputConfirm = $('#')
+}
+textConfirm()
